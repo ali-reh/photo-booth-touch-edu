d7daf53c355e30480b598f636a5d3d93ab61bf9d
ali-reh <alirehawi1221@gmail.com>
2026-09-02 17:13:19 +0300

    added

--- scripts/download-models.mjs
+++ scripts/download-models.mjs
@@ -7,13 +7,11 @@ const TARGET_DIR = path.join(process.cwd(), 'public', 'models');
 
 const MODEL_FILES = [
   'ssd_mobilenetv1_model-weights_manifest.json',
-  'ssd_mobilenetv1_model-shard1',
-  'ssd_mobilenetv1_model-shard2',
+  'ssd_mobilenetv1_model.bin',
   'face_landmark_68_model-weights_manifest.json',
-  'face_landmark_68_model-shard1',
+  'face_landmark_68_model.bin',
   'face_recognition_model-weights_manifest.json',
-  'face_recognition_model-shard1',
-  'face_recognition_model-shard2',
+  'face_recognition_model.bin',
 ];
 
 function downloadFile(url, destPath, maxRedirects = 5) {
